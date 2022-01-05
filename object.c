8543eda9cc4c392ae486af0ef21e678d7e664339
David Moss <drkjam@gmail.com>
2022-01-05 22:11:28 +0000

    - added basic support for native functions

--- object.c
+++ object.c
@@ -28,6 +28,12 @@ ObjFunction* newFunction() {
     return function;
 }
 
+ObjNative* newNative(NativeFn function) {
+    ObjNative* native = ALLOCATE_OBJ(ObjNative, OBJ_NATIVE);
+    native->function = function;
+    return native;
+}
+
 static uint32_t hashString(const char* key, int length) {
     //  Use the FNV-1a hashing algorithm.
     uint32_t hash = 216613626lu;
@@ -82,6 +88,9 @@ void printObject(Value value) {
     switch (OBJ_TYPE(value)) {
         case OBJ_FUNCTION:
             printFunction(AS_FUNCTION(value));
+            break;
+        case OBJ_NATIVE:
+            printf("native fn");
             break;
         case OBJ_STRING:
             printf("%s", AS_CSTRING(value));
